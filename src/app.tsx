0d957e025add8f37cb6cd2c05e77490cb38971a9
sagar-pardhi <sgrpardhi007@gmail.com>
2023-09-06 14:07:04 +0530

    added quiz component [skip actions]

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,70 @@
+import { useState } from "react";
+import { Quiz } from "./components/quiz";
+
 export const App = () => {
+  const [questionsData, setQuestionsData] = useState([
+    {
+      capital: "New Delhi",
+      options: [
+        { answer: "India", isCorrect: true },
+        { answer: "China", isCorrect: false },
+        { answer: "Pakistan", isCorrect: false },
+        { answer: "Bangladesh", isCorrect: false },
+      ],
+    },
+    {
+      capital: "Washington D.C",
+      options: [
+        { answer: "England", isCorrect: false },
+        { answer: "USA", isCorrect: true },
+        { answer: "Italy", isCorrect: false },
+        { answer: "France", isCorrect: false },
+      ],
+    },
+    {
+      capital: "Kathmandu",
+      options: [
+        { answer: "India", isCorrect: false },
+        { answer: "China", isCorrect: false },
+        { answer: "Nepal", isCorrect: true },
+        { answer: "Bangladesh", isCorrect: false },
+      ],
+    },
+    {
+      capital: "Tokyo",
+      options: [
+        { answer: "India", isCorrect: false },
+        { answer: "Australia", isCorrect: false },
+        { answer: "South Korea", isCorrect: false },
+        { answer: "Japan", isCorrect: true },
+      ],
+    },
+  ]);
+  const [currentQuestion, setCurrentQuestion] = useState(questionsData[0]);
+  const [questionIndex, setQuestionIndex] = useState(1);
+  const [score, setScore] = useState(0);
+
+  console.log("👉 ", {
+    questionIndex,
+    questionsDataLength: questionsData.length,
+  });
+
+  const handleNext = () => {
+    if (questionIndex === questionsData.length) {
+      return;
+    }
+    setQuestionIndex((prev) => prev + 1);
+    setCurrentQuestion(questionsData[questionIndex]);
+  };
+
   return (
-    <>
-      <h1 className="text-3xl">Hello, Vite.js</h1>
-    </>
+    <main className="container mx-auto flex justify-center items-center h-screen w-full">
+      <img
+        src="/assets/background.png"
+        alt="background image"
+        className="absolute w-full h-screen object-cover -z-10"
+      />
+      <Quiz {...currentQuestion} handleNext={handleNext} />
+    </main>
   );
 };
